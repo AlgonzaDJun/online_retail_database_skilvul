60f246ab97f1362a0786056bba0bef5b5ba11679
AlgonzaDJun <algonsadewangga@gmail.com>
2023-11-07 15:09:46 +0700

    finish create table

--- create_table.sql
+++ create_table.sql
@@ -39,3 +39,29 @@ CREATE TABLE barang (
    Foreign Key (id_kategori) REFERENCES kategori(id_kategori)
 );
 
+desc barang;
+
+-- buat table orders
+CREATE TABLE orders (
+   id_order int  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   id_user int  NOT NULL,
+   total_harga int  NOT NULL,
+   tanggal datetime  NOT NULL,
+   Foreign Key (id_user) REFERENCES user(id_user)
+);
+
+DESC orders;
+
+-- buat table order_detail
+CREATE TABLE order_detail (
+   id_detail_order int  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   id_order int  NOT NULL,
+   id_barang int  NOT NULL,
+   kuantitas int  NOT NULL,
+   Foreign Key (id_order) REFERENCES orders(id_order),
+   Foreign Key (id_barang) REFERENCES barang(id_barang)
+);
+
+desc order_detail;
+
+show tables;
