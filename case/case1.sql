53a2fdc14bafbc770f01b55d5f12d8389828f4b2
AlgonzaDJun <algonsadewangga@gmail.com>
2023-11-09 14:55:06 +0700

    case 1 and revisi case 3

---
+++ case/case1.sql
@@ -0,0 +1,3 @@
+SELECT * from orders;
+
+INSERT INTO orders (id_user, total_harga, tanggal) VALUES (1, 200000, '2020-12-12');
--- case/case3.sql
+++ case/case3.sql
@@ -1,7 +1,7 @@
 SELECT
     kategori.nama_kategori,
-    COUNT(id_barang) as jumlah_barang
+    COUNT(barang.id_barang) as jumlah_barang
 FROM barang
     JOIN kategori ON barang.id_kategori = kategori.id_kategori
 GROUP BY barang.id_kategori
-ORDER BY jumlah_barang DESC;
+ORDER BY jumlah_barang DESC LIMIT 1;
